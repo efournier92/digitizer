5810406d019a3a5479059531100557f7be6a82e9
efournier92 <efournier92@gmail.com>
2021-04-25 00:44:08 -0400

    add dynamic directory creation in batch mode

--- _src/modes/batch_mode.bash
+++ _src/modes/batch_mode.bash
@@ -8,6 +8,24 @@
 
 source "./_src/messages/errors.bash"
 
+get_output_dir() {
+  [[ "$VERBOSE" = true ]] && log_arguments "${FUNCNAME[0]}" "$@"
+  local ffmpeg_command="$1"
+
+  local output_location=`echo $ffmpeg_command | awk '{print $NF}'`
+
+  echo ${output_location%/*}
+}
+
+create_output_dir() {
+  [[ "$VERBOSE" = true ]] && log_arguments "${FUNCNAME[0]}" "$@"
+  local ffmpeg_command="$1"
+
+  local output_dir=`get_output_dir "$ffmpeg_command"`
+  
+  mkdir "$output_dir"
+}
+
 batch_mode() {
   [[ "$VERBOSE" = true ]] && log_arguments "${FUNCNAME[0]}" "$@"
   local batch_file="$1"
@@ -15,8 +33,9 @@ batch_mode() {
   [[ -z "$batch_file" ]] && error_missing_function_args "${FUNCNAME[0]}" "$@"
   [[ ! -f "$batch_file" ]] && error_file_not_found "$batch_file" `batch_mode_name`
 
-  while IFS= read -r command
-  do
+  while IFS= read -r command; do
+    create_output_dir "$command"
+
     eval "$command < /dev/null"
   done < "$batch_file"
 }
